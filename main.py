2a6bba225b5e374388688622e5bf4f040827247f
V L <83759947+chessmaster987@users.noreply.github.com>
2023-03-18 22:14:19 +0200

    add a asteroid and ufo and its relations
    Added a UFO and Asteroids objects. Asteroids are enemies. They createed in random place in right side of game screen

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import random as rd
 from random import randrange
 
 pygame.init()
-pygame.display.set_caption('Flappy Goose')
+pygame.display.set_caption('UFO vs Asteroids')
+
+FPS = pygame.time.Clock()
 
 WHITE = (255, 255, 255)
 
@@ -11,25 +13,50 @@ screen = width, height = (1200, 700)
 
 main_window = pygame.display.set_mode(screen)
 
-obj = pygame.Surface((20, 20))
-obj.fill(WHITE)
-obj_rect = obj.get_rect()
-obj_speed = [1, 1]
+image_obj = pygame.image.load('./imgs/ufo.jpg')
+image_enemy = pygame.image.load('./imgs/asteroid.jpg')
+
+obj_x = width // 2
+obj_y = height // 2
+obj_speed = 5
+
+def create_enemy():
+   enemy = pygame.Surface((20, 20))
+   enemy_rect = pygame.Rect(width, rd.randint(0, height), *enemy.get_size())
+   enemy_speed = rd.randint(5, 10)
+   return [enemy, enemy_rect, enemy_speed]
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, 750) #time of creating asteroids
+  
+enemies = []
 
 running = True
-
 while running:
+   FPS.tick(60)
    for event in pygame.event.get():
       if event.type == pygame.QUIT:
          running = False 
-   rand_color = (randrange(255), randrange(255), randrange(255))
-   obj_rect = obj_rect.move(obj_speed)
-   if obj_rect.bottom >= height or obj_rect.top <= 0:
-      obj_speed[1] = -obj_speed[1]
-      obj.fill(rand_color)
-   if obj_rect.right >= width or obj_rect.left <= 0:
-      obj_speed[0] = -obj_speed[0]
-      obj.fill(rand_color)
-   main_window.fill((155, 155, 155))
-   main_window.blit(obj, obj_rect)
+      if event.type == CREATE_ENEMY:
+         enemies.append(create_enemy())
+
+   main_window.fill((WHITE))
+   main_window.blit(image_obj, (obj_x, obj_y))
+
+   keys = pygame.key.get_pressed()
+   if keys[pygame.K_LEFT]:
+      obj_x -= obj_speed
+   if keys[pygame.K_RIGHT]:
+      obj_x += obj_speed
+   if keys[pygame.K_UP]:
+      obj_y -= obj_speed
+   if keys[pygame.K_DOWN]:
+      obj_y += obj_speed
+   
+   for enemy in enemies:
+      enemy[1] = enemy[1].move(-enemy[2], 0)
+      main_window.blit(image_enemy, enemy[1])
+      if enemy[1].left < 0:
+         enemies.pop(enemies.index(enemy)) #clear the left asteroids
+         
    pygame.display.flip()
